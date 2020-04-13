e8374d0bc131578d0da8c1b1835720bda7fa5af0
Boris Osipov <osipov.boris@gmail.com>
2020-04-13 21:34:52 +0300

    fix to new api

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ const deleteMessage = async (tgMessage) => {
     try {
         if (!tgMessage.new_chat_members) {
             await bot.deleteMessage(tgMessage.chat.id, tgMessage.message_id.toString());
+            console.log(`delete join message ${tgMessage.chat.id}`)
         }
     } catch (e) {
         console.error(e);
     }
 };
 
+const deleteJoin = async (tgMessage) => {
+    try {
+        await bot.deleteMessage(tgMessage.chat.id, tgMessage.message_id.toString());
+        console.log(`delete join ${tgMessage.chat.id}`)
+    } catch (e) {
+        console.error(e);
+    }
+};
+
 bot.on('message', deleteMessage);
+bot.on('new_chat_members', deleteJoin);
